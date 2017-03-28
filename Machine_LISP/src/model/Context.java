86f3a274fc389cbb91802e32a0144028c5ea0c0a
Virgil BONTEMPI <virgilbontempi@hotmail.fr>
2017-03-28 13:56:34 +0200

    correction

--- Machine_LISP/src/model/Context.java
+++ Machine_LISP/src/model/Context.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Context {
 
-	private static Map<Symbole, SExpr> table = new HashMap<>();
+	private static Map<SExpr, SExpr> table = new HashMap<>();
 	
 	public static SExpr getSexpr(Symbole symbole){
 		if(!table.containsKey(symbole)) throw new LispException("Variable indéfinie");
@@ -14,14 +14,17 @@ public class Context {
 	
 	public static void bind1(SExpr sExprFormel, SExpr sExprEffectif){
 		if(sExprFormel instanceof Symbole){
-			
+			table.put(sExprFormel.car(), new SCons(sExprEffectif.car(),sExprEffectif.cdr()));
 		}
-		if(sExprFormel.atom() && sExprEffectif.atom()){
-			table.put(sExprFormel, sExprEffectif);
+		else{
+			bind2(sExprFormel,sExprEffectif);
 		}
+		
 	}
 	
 	public static void bind2(){
-		
+		if(sExprFormel.atom() && sExprEffectif.atom()){
+			table.put(sExprFormel, sExprEffectif);
+		}
 	}
 }
